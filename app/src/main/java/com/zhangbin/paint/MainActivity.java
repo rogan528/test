6e6c7de9c2b515ba0733ccd3579b3e5a51461a57
张彬 <zhangbin@haixun360.com>
2019-04-23 18:15:31 +0800

    下面的返回键的处理

--- app/src/main/java/com/zhangbin/paint/MainActivity.java
+++ app/src/main/java/com/zhangbin/paint/MainActivity.java
@@ -394,4 +394,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
             }
         }, 100);
     }
+    @Override
+    public void onBackPressed() {
+        gobackAction();
+    }
 }
