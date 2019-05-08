1873cf1a1992de836137d762f9f69a50ddb500c0
张彬 <zhangbin@haixun360.com>
2019-05-08 16:30:56 +0800

    显示视频第一帧和去掉重力旋转

--- app/src/main/java/com/zhangbin/paint/whiteboard/WhiteDrawView.java
+++ app/src/main/java/com/zhangbin/paint/whiteboard/WhiteDrawView.java
@@ -73,7 +73,7 @@ public final class WhiteDrawView extends FrameLayout {
     }
 
     private void initWebSetting() {
-        String url = "http://www.baidu.com/";
+        String url = "http://192.168.8.37:8081/83461B08A0401FC68D9C2A7E036C4710/h5/h5.html?aaaa";
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
